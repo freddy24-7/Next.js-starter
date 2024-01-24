74cfb41cbb7f1e180056e996c4e88c7a2b2930a4
freddy24-7 <freddy.austli@aol.com>
2024-01-24 11:12:05 +0100

    adding to git

--- app/page.js
+++ app/page.js
@@ -1,25 +1,14 @@
-// import Link from 'next/link';
-//
-// export default function Home() {
-//     return (
-//         <main>
-//             <img src="/icon.png" alt="A server surrounded by magic sparkles." />
-//             <h1>Welcome to this NextJS Course!</h1>
-//             <p>🔥 Let&apos;s get started! 🔥</p>
-//             <p><Link href="/about">About Us</Link></p>
-//         </main>
-//     );
-// }
 import Link from 'next/link';
 
-import Header from '@/components/header';
-
 export default function Home() {
-    return (
-        <main>
-            <Header />
-            <p>🔥 Let&apos;s get started! 🔥</p>
-            <p><Link href="/about">About Us</Link></p>
-        </main>
-    );
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
+      <p><Link href="/meals">Meals</Link></p>
+      <p><Link href="/meals/share">Share Meal</Link></p>
+      <p><Link href="/community">Community</Link></p>
+    </main>
+  );
 }
